ba362909d34ff920814a1b427a69d4617a8bd6a9
SwathyJagannatha <swathyjagannatha308@gmail.com>
2024-06-10 13:12:43 -0700

    readme file

---
+++ homework/indent.py
@@ -0,0 +1,9 @@
+# print("Python Homework Week2 - Day 1")
+# Python Indentation Practice with if Statements
+
+weather = "sunny"
+
+if weather == "sunny":
+    print("Wear sunglasses!")
+else:
+    print("Take an umbrella!")
---
+++ homework/mood.py
@@ -0,0 +1,11 @@
+# Task 2: Your Mood Today  
+
+print('Hello,Please input your mood')
+usermood = input()
+
+if usermood == 'happy':
+    print("That's great to hear!")
+elif usermood == 'sad':
+    print("I hope your day gets better!")
+
+
